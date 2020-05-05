5937140d3a7123012c3d64c1bc5e48a9247f170f
Abhinav Pappu <abhi1pappu9@gmail.com>
2020-05-05 03:06:45 -0500

    Add movie search page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import MovieSearch from 'pages/MovieSearch';
+import Home from 'pages/Home';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+
+          <Route path="/movies" exact>
+            <MovieSearch />
+          </Route>
+
+          <Route path="/movies/:asin">
+            
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/MovieSearch/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Select from 'react-select';
+import { useHistory } from 'react-router-dom';
+
+import movies from 'constants/movies.json';
+import styles from './styles.module.scss';
+
+function MovieSearch() {
+  const history = useHistory();
+
+  const options = Object.entries(movies).map(([asin, { title, year }]) => ({
+    label: `${title} (${year})`,
+    value: asin,
+  }));
+
+  return (
+    <div className={styles.MovieSearch}>
+      <Select
+        className={styles.movieSelect}
+        placeholder="Select a movie..."
+        onChange={({ value }) => history.push(`/movies/${value}`)}
+        options={options}
+      />
+    </div>
+  );
+}
+
+export default MovieSearch;
